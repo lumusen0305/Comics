3bf6db4238f96fc78296df4527162dffc5eeddda
lumusen0305 <lumusen890305@gmail.com>
2021-07-19 18:52:51 +0800

    set introduce slider

--- src/application/about/about.js
+++ src/application/about/about.js
@@ -2,9 +2,25 @@ import Vue from 'vue'
 import App from './about.vue'
 import 'ant-design-vue/dist/antd.css';
 import { Button } from 'ant-design-vue';
+import { Layout } from 'ant-design-vue';
+import { Icon } from 'ant-design-vue';
+import { Col } from 'ant-design-vue';
+import { Modal } from 'ant-design-vue';
+import { Card } from 'ant-design-vue';
+import { Input } from 'ant-design-vue';
+import { Select } from 'ant-design-vue';
+import { Carousel} from 'ant-design-vue';
 Vue.config.productionTip = false
 
+Vue.use(Carousel);
+Vue.use(Input);
+Vue.use(Select);
+Vue.use(Modal);
+Vue.use(Card);
+Vue.use(Col);
+Vue.use(Icon);
 Vue.use(Button);
+Vue.use(Layout);
 
 new Vue({
     render: h => h(App)
--- src/application/about/about.vue
+++ src/application/about/about.vue
@@ -1,9 +1,18 @@
 <template>
-  <div>
-
+  <div class="bg">
+    <a-layout id="components-layout-demo-basic">
+      <a-layout-sider width="40%">
+        <img src="../../assets/anemia_1.png">
+      </a-layout-sider>
+      <a-layout>
+        <a-layout-header>
+          <StarOutlined />
+        </a-layout-header>
+        <a-layout-content>Content</a-layout-content>
+      </a-layout>
+    </a-layout>
   </div>
 </template>
-
 
 <script>
 export default {
@@ -12,5 +21,40 @@ export default {
 </script>
 
 <style scoped>
+html,body {
+  height: 100%;
+  padding: 0;
+  margin: 0;
+}
+.ant-layout-sider,.ant-layout-header,.ant-layout{
+  background-color: #F3CD05;
+}
+#components-layout-demo-basic .ant-layout-sider img {
+  min-height: 100%;
+  width: 100%;
+}
+#components-layout-demo-basic {
+  text-align: center;
+  height: 100vh;
+  width: 100%;
+}
+#components-layout-demo-basic .ant-layout-header{
+  color: #fff;
+}
+#components-layout-demo-basic .ant-layout-sider {
+  color: #fff;
+  line-height: 120px;
+}
+#components-layout-demo-basic .ant-layout-content {
+  color: #fff;
+  min-height: 120px;
+  line-height: 120px;
+}
+#components-layout-demo-basic > .ant-layout {
+  margin-bottom: 48px;
+}
+#components-layout-demo-basic > .ant-layout:last-child {
+  margin: 0;
+}
 
 </style>
